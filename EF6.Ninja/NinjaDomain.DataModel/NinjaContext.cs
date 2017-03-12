dc8a84d31c84eb938a68ae30bdfabe8e7e4e3b89
Zayn An Nisa <shobuz_cse03@yahoo.com>
2017-03-12 22:42:46 +0600

    update from home

--- EF6.Ninja/NinjaDomain.DataModel/NinjaContext.cs
+++ EF6.Ninja/NinjaDomain.DataModel/NinjaContext.cs
@@ -1,5 +1,8 @@
-﻿using NinjaDomain.Classes;
+﻿using System;
+using NinjaDomain.Classes;
 using System.Data.Entity;
+using System.Linq;
+using NinjaDomain.Classes.Interfaces;
 
 namespace NinjaDomain.DataModel
 {
@@ -18,8 +21,32 @@ namespace NinjaDomain.DataModel
         public DbSet<NinjaEquipment> Equipment { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            //modelBuilder.Types().Configure(c=>c.Ignore("IsDirty"));
+            modelBuilder.Types().Configure(c=>c.Ignore("IsDirty"));
             base.OnModelCreating(modelBuilder);
+        }
+        public override int SaveChanges()
+        {
+            foreach (var history in this.ChangeTracker.Entries()
+              .Where(e => e.Entity is IModificationHistory && (e.State == EntityState.Added ||
+                      e.State == EntityState.Modified))
+               .Select(e => e.Entity as IModificationHistory)
+              )
+            {
+                history.DateModified = DateTime.Now;
+                if (history.DateCreated == DateTime.MinValue)
+                {
+                    history.DateCreated = DateTime.Now;
+                }
+            }
+            int result = base.SaveChanges();
+            foreach (var history in this.ChangeTracker.Entries()
+                                          .Where(e => e.Entity is IModificationHistory)
+                                          .Select(e => e.Entity as IModificationHistory)
+              )
+            {
+                history.IsDirty = false;
+            }
+            return result;
         }
     }
 }
